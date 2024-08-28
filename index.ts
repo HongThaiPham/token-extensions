ae6831a002779611b48cf9c9815692801e44d879
leopham <hongthaipro@gmail.com>
2024-08-28 14:32:21 +0700

    Update .gitignore to include **/test-ledger/** Update package.json to add @solana/spl-token dependency

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
+import {
+  createMint,
+  getMint,
+  getOrCreateAssociatedTokenAccount,
+  mintTo,
+  TOKEN_2022_PROGRAM_ID,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
 import transferSol from "./examples/transfer-sol";
-import { payer, testWallet } from "./lib/config";
+import { connection, payer, testWallet } from "./lib/config";
 import { airdropToWallet } from "./lib/wallet";
 
 (async () => {
@@ -8,6 +16,75 @@ import { airdropToWallet } from "./lib/wallet";
     "Test Wallet": testWallet.publicKey.toBase58(),
   });
 
-  await airdropToWallet(payer.publicKey, 1000000000);
-  await transferSol(payer, testWallet.publicKey, 1000000);
+  airdropToWallet(payer.publicKey, 1000000000);
+  // airdropToWallet(testWallet.publicKey, 1000000000);
+
+  const mint = await createMint(
+    connection,
+    payer,
+    payer.publicKey,
+    null,
+    9,
+    undefined,
+    {
+      commitment: "finalized",
+    },
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  console.log("Fetching mint info...");
+
+  const mintInfo = await getMint(
+    connection,
+    mint,
+    "finalized",
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  console.log("Mint Info", mintInfo);
+
+  const tokenAccount = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    mint,
+    payer.publicKey,
+    false,
+    "finalized",
+    { commitment: "finalized" },
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  await mintTo(
+    connection,
+    payer,
+    mint,
+    tokenAccount.address,
+    payer.publicKey,
+    1000_000_000_000,
+    [],
+    { commitment: "finalized" },
+    TOKEN_2022_PROGRAM_ID
+  );
+
+  const tokenAccounts = await getTokenAccountsByOwner();
+
+  console.table(tokenAccounts);
 })();
+
+async function getTokenAccountsByOwner() {
+  const { value: tokenAccounts } = await connection.getTokenAccountsByOwner(
+    payer.publicKey,
+    {
+      programId: TOKEN_PROGRAM_ID,
+    }
+  );
+
+  const { value: tokenExtAccounts } = await connection.getTokenAccountsByOwner(
+    payer.publicKey,
+    {
+      programId: TOKEN_2022_PROGRAM_ID,
+    }
+  );
+
+  return [...tokenAccounts, ...tokenExtAccounts];
+}
